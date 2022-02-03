a371be5f1a80ffa23e36a026bb3513ff54b326ae
Durgesh Tanwar <durgeshtanwar@Durgeshs-MacBook-Pro.local>
2022-02-03 21:59:55 +0530

    scraped data saved in json

--- pageController.js
+++ pageController.js
@@ -1,4 +1,5 @@
 const pageScraper = require("./pageScraper");
+const fs = require("fs");
 async function scrapeAll(browserInstance) {
   let browser;
   try {
@@ -6,10 +7,27 @@ async function scrapeAll(browserInstance) {
     let scrapedData = {};
     // Call the scraper for different set of books to be scraped
     scrapedData["Travel"] = await pageScraper.scraper(browser, "Travel");
+    scrapedData["HistoricalFiction"] = await pageScraper.scraper(
+      browser,
+      "Historical Fiction"
+    );
+    scrapedData["Mystery"] = await pageScraper.scraper(browser, "Mystery");
     await browser.close();
+    fs.writeFile(
+      "data.json",
+      JSON.stringify(scrapedData),
+      "utf-8",
+      function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("The file was saved!");
+      }
+    );
     console.log(scrapedData);
   } catch (err) {
     console.log("Could not resolve the browser instance => ", err);
   }
 }
+
 module.exports = (browserInstance) => scrapeAll(browserInstance);
