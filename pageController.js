af2097b9186838e434e79e4e170b20824106098c
Durgesh Tanwar <durgeshtanwar@Durgeshs-MacBook-Pro.local>
2022-02-03 21:50:05 +0530

    category wise search

--- pageController.js
+++ pageController.js
@@ -1,14 +1,15 @@
 const pageScraper = require("./pageScraper");
-
 async function scrapeAll(browserInstance) {
   let browser;
   try {
     browser = await browserInstance;
-    await pageScraper.scraper(browser);
+    let scrapedData = {};
+    // Call the scraper for different set of books to be scraped
+    scrapedData["Travel"] = await pageScraper.scraper(browser, "Travel");
+    await browser.close();
+    console.log(scrapedData);
   } catch (err) {
-    console.log("Error scraping: " + err);
+    console.log("Could not resolve the browser instance => ", err);
   }
 }
-module.exports = (browserInstance) => {
-  scrapeAll(browserInstance);
-};
+module.exports = (browserInstance) => scrapeAll(browserInstance);
